c76b150931f9a4e8c00be27bb02dd97c7e29156e
Leonard Thieu <leonard.thieu@gmail.com>
2018-08-20 10:01:10 -0400

    Customize serialization of Node types.

--- src/Node.ts
+++ src/Node.ts
@@ -4,10 +4,34 @@ import { MissingToken } from './MissingToken';
 export abstract class Node {
     parent: Node | null = null;
     children: Array<Token | Node> = [];
+
+    toJSON() {
+        return {
+            [this.constructor.name]: {
+                ...this.toJSONBeforeChildren(),
+                children: this.children,
+                ...this.toJSONAfterChildren(),
+            },
+        };
+    }
+
+    protected toJSONBeforeChildren(): any {
+        return null;
+    }
+
+    protected toJSONAfterChildren(): any {
+        return null;
+    }
 }
 
 export class ModuleNode extends Node {
     eofToken: Token | null = null;
+
+    protected toJSONAfterChildren() {
+        return {
+            eofToken: this.eofToken,
+        };
+    }
 }
 
 export class IfDirectiveNode extends Node {
@@ -16,28 +40,82 @@ export class IfDirectiveNode extends Node {
     elseIfDirectives: ElseIfDirectiveNode[] = [];
     elseDirective: ElseDirectiveNode | null = null;
     endDirectiveKeyword: Token | MissingToken | null = null;
+
+    protected toJSONBeforeChildren() {
+        return {
+            ifDirectiveKeyword: this.ifDirectiveKeyword,
+            expression: this.expression,
+        };
+    }
+
+    protected toJSONAfterChildren() {
+        return {
+            elseIfDirectives: this.elseIfDirectives,
+            elseDirective: this.elseDirective,
+            endDirectiveKeyword: this.endDirectiveKeyword,
+        };
+    }
 }
 
 export class ElseIfDirectiveNode extends Node {
     elseIfDirectiveKeyword: Token | null = null;
     expression: Node | MissingToken | null = null;
+
+    protected toJSONBeforeChildren() {
+        return {
+            elseIfDirectiveKeyword: this.elseIfDirectiveKeyword,
+            expression: this.expression,
+        };
+    }
 }
 
 export class ElseDirectiveNode extends Node {
     elseDirectiveKeyword: Token | null = null;
+
+    protected toJSONBeforeChildren() {
+        return {
+            elseDirectiveKeyword: this.elseDirectiveKeyword,
+        };
+    }
 }
 
 export class RemDirectiveNode extends Node {
     remDirectiveKeyword: Token | null = null;
     endDirectiveKeyword: Token | MissingToken | null = null;
+
+    protected toJSONBeforeChildren() {
+        return {
+            remDirectiveKeyword: this.remDirectiveKeyword,
+        };
+    }
+
+    protected toJSONAfterChildren() {
+        return {
+            endDirectiveKeyword: this.endDirectiveKeyword,
+        };
+    }
 }
 
 export class PrintDirectiveNode extends Node {
     printDirectiveKeyword: Token | null = null;
     expression: Node | MissingToken | null = null;
+
+    protected toJSONBeforeChildren() {
+        return {
+            printDirectiveKeyword: this.printDirectiveKeyword,
+            expression: this.expression,
+        };
+    }
 }
 
 export class ErrorDirectiveNode extends Node {
     errorDirectiveKeyword: Token | null = null;
     expression: Node | MissingToken | null = null;
+
+    protected toJSONBeforeChildren() {
+        return {
+            errorDirectiveKeyword: this.errorDirectiveKeyword,
+            expression: this.expression,
+        };
+    }
 }
