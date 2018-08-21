8ec45aecdc39b299f853b74bbbeb3a13b8f0858b
Leonard Thieu <leonard.thieu@gmail.com>
2018-08-21 09:16:03 -0400

    Improve serialization of Node objects.

--- src/Node.ts
+++ src/Node.ts
@@ -6,135 +6,110 @@ export abstract class Node {
     children: Array<Token | Node> = [];
 
     toJSON() {
+        const obj: any = {};
+
+        for (const childName of this.getChildNames()) {
+            obj[childName] = this[childName];
+        }
+
         return {
-            [this.constructor.name]: {
-                ...this.toJSONBeforeChildren(),
-                children: this.children,
-                ...this.toJSONAfterChildren(),
-            },
+            [this.constructor.name]: obj,
         };
     }
 
-    protected toJSONBeforeChildren(): any {
-        return null;
-    }
+    private getChildNames<K extends keyof this>(): K[] {
+        const ctor = this.constructor as any;
 
-    protected toJSONAfterChildren(): any {
-        return null;
+        return ctor.CHILD_NAMES || [];
     }
 }
 
 export class ModuleNode extends Node {
-    eofToken: Token | null = null;
+    static CHILD_NAMES: (keyof ModuleNode)[] = [
+        'children',
+        'eofToken',
+    ];
 
-    protected toJSONAfterChildren() {
-        return {
-            eofToken: this.eofToken,
-        };
-    }
+    eofToken: Token | null = null;
 }
 
 export class IfDirectiveNode extends Node {
+    static CHILD_NAMES: (keyof IfDirectiveNode)[] = [
+        'ifDirectiveKeyword',
+        'expression',
+        'children',
+        'elseIfDirectives',
+        'elseDirective',
+        'endDirectiveKeyword',
+    ];
+
     ifDirectiveKeyword: Token | null = null;
     expression: Expression | MissingToken | null = null;
     elseIfDirectives: ElseIfDirectiveNode[] = [];
     elseDirective: ElseDirectiveNode | null = null;
     endDirectiveKeyword: Token | MissingToken | null = null;
-
-    protected toJSONBeforeChildren() {
-        return {
-            ifDirectiveKeyword: this.ifDirectiveKeyword,
-            expression: this.expression,
-        };
-    }
-
-    protected toJSONAfterChildren() {
-        return {
-            elseIfDirectives: this.elseIfDirectives,
-            elseDirective: this.elseDirective,
-            endDirectiveKeyword: this.endDirectiveKeyword,
-        };
-    }
 }
 
 export class ElseIfDirectiveNode extends Node {
+    static CHILD_NAMES: (keyof ElseIfDirectiveNode)[] = [
+        'elseIfDirectiveKeyword',
+        'expression',
+        'children',
+    ];
+
     elseIfDirectiveKeyword: Token | null = null;
     expression: Expression | MissingToken | null = null;
-
-    protected toJSONBeforeChildren() {
-        return {
-            elseIfDirectiveKeyword: this.elseIfDirectiveKeyword,
-            expression: this.expression,
-        };
-    }
 }
 
 export class ElseDirectiveNode extends Node {
-    elseDirectiveKeyword: Token | null = null;
+    static CHILD_NAMES: (keyof ElseDirectiveNode)[] = [
+        'elseDirectiveKeyword',
+        'children',
+    ];
 
-    protected toJSONBeforeChildren() {
-        return {
-            elseDirectiveKeyword: this.elseDirectiveKeyword,
-        };
-    }
+    elseDirectiveKeyword: Token | null = null;
 }
 
 export class RemDirectiveNode extends Node {
+    static CHILD_NAMES: (keyof RemDirectiveNode)[] = [
+        'remDirectiveKeyword',
+        'children',
+        'endDirectiveKeyword',
+    ];
+
     remDirectiveKeyword: Token | null = null;
     endDirectiveKeyword: Token | MissingToken | null = null;
-
-    protected toJSONBeforeChildren() {
-        return {
-            remDirectiveKeyword: this.remDirectiveKeyword,
-        };
-    }
-
-    protected toJSONAfterChildren() {
-        return {
-            endDirectiveKeyword: this.endDirectiveKeyword,
-        };
-    }
 }
 
 export class PrintDirectiveNode extends Node {
+    static CHILD_NAMES: (keyof PrintDirectiveNode)[] = [
+        'printDirectiveKeyword',
+        'expression',
+    ];
+
     printDirectiveKeyword: Token | null = null;
     expression: Expression | MissingToken | null = null;
-
-    protected toJSONBeforeChildren() {
-        return {
-            printDirectiveKeyword: this.printDirectiveKeyword,
-            expression: this.expression,
-        };
-    }
 }
 
 export class ErrorDirectiveNode extends Node {
+    static CHILD_NAMES: (keyof ErrorDirectiveNode)[] = [
+        'errorDirectiveKeyword',
+        'expression',
+    ];
+
     errorDirectiveKeyword: Token | null = null;
     expression: Expression | MissingToken | null = null;
-
-    protected toJSONBeforeChildren() {
-        return {
-            errorDirectiveKeyword: this.errorDirectiveKeyword,
-            expression: this.expression,
-        };
-    }
 }
 
 export abstract class Expression extends Node { }
 
 export class StringLiteral extends Expression {
+    static CHILD_NAMES: (keyof StringLiteral)[] = [
+        'startQuote',
+        'children',
+        'endQuote',
+    ];
+
     startQuote: Token | null = null;
     endQuote: Token | MissingToken | null = null;
-
-    protected toJSONBeforeChildren() {
-        return {
-            startQuote: this.startQuote,
-        };
-    }
-
-    protected toJSONAfterChildren() {
-        return {
-            endQuote: this.endQuote,
-        };
-    }
 }
