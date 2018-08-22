a2d147434c3a77f8edb5e60d9ca723622baecf56
Leonard Thieu <leonard.thieu@gmail.com>
2018-08-22 04:59:33 -0400

    Change test infrastructure to allow testing external Monkey X code.

---
+++ tests/MonkeyTests.ts
@@ -0,0 +1,36 @@
+import path = require('path');
+
+import { PreprocessorParser } from '../src/PreprocessorParser';
+import { Token, TokenKind } from '../src/Token';
+import { Tokenizer } from '../src/Tokenizer';
+import { executeTestCases } from "./shared";
+
+executeTestCases({
+    name: 'MonkeyX',
+    casesPath: path.resolve('..', '..', 'blitz-research', 'monkey', 'modules'),
+    outputBasePath: path.join(__dirname, 'cases', 'MonkeyX'),
+    ext: 'tokens',
+    testCallback: (contents) => {
+        const tokenizer = new Tokenizer(contents);
+        const tokens: Token[] = [];
+        let t: Token;
+        do {
+            t = tokenizer.next();
+            tokens.push(t);
+        } while (t.kind !== TokenKind.EOF);
+    
+        return tokens;
+    },
+});
+
+executeTestCases({
+    name: 'MonkeyX',
+    casesPath: path.resolve('..', '..', 'blitz-research', 'monkey', 'modules'),
+    outputBasePath: path.join(__dirname, 'cases', 'MonkeyX'),
+    ext: 'tree',
+    testCallback: (contents) => {
+        const parser = new PreprocessorParser();
+        
+        return parser.parse(contents);
+    },
+});
--- tests/shared.ts
+++ tests/shared.ts
@@ -1,22 +1,44 @@
 import glob = require('glob');
 import fs = require('fs');
 import path = require('path');
+import mkdirp = require('mkdirp');
 import { orderBy } from 'natural-orderby';
 
-export function executeTestCases(type: string, ext: string, testCallback: TestCallback) {
-    const skippedCases: string[] = require(`./${type}.skipped.json`);
+export function executeTestCases(options: TestCaseOptions): void;
+export function executeTestCases(type: string, ext: string, testCallback: TestCallback): void;
 
-    describe(type, function () {
-        const casesPath = path.join(__dirname, 'cases', type);
-        const casesGlobPath = path.join(casesPath, '*.monkey');
+export function executeTestCases(type: TestCaseOptions | string, ext?: string, testCallback?: TestCallback): void {
+    let name: string;
+    let casesPath: string;
+    let outputBasePath: string;
+    let skippedCases: string[] | undefined;
+
+    if (typeof type !== 'string') {
+        const options = type;
+        name = options.name;
+        casesPath = options.casesPath;
+        outputBasePath = options.outputBasePath;
+        ext = options.ext;
+        testCallback = options.testCallback;
+    } else {
+        name = type;
+        casesPath = path.join(__dirname, 'cases', type);
+        outputBasePath = casesPath;
+        skippedCases = require(`./${type}.skipped.json`);
+    }
+
+    describe(name, function () {
+        const casesGlobPath = path.join(casesPath, '**', '*.monkey');
         const sourcePaths = orderBy(glob.sync(casesGlobPath));
     
         for (const sourcePath of sourcePaths) {
             const sourceBasename = path.basename(sourcePath);
-            const outputPath = sourcePath + `.${ext}.json`;
+            const outputDir = path.join(outputBasePath, path.relative(casesPath, path.dirname(sourcePath)));
+            mkdirp.sync(outputDir);
+            const outputPath = path.join(outputDir, `${sourceBasename}.${ext}.json`);
     
             let _it: Mocha.PendingTestFunction = it;
-            if (skippedCases.includes(sourceBasename)) {
+            if (skippedCases && skippedCases.includes(sourceBasename)) {
                 _it = xit;
                 try {
                     fs.unlinkSync(outputPath);
@@ -34,13 +56,21 @@ export function executeTestCases(type: string, ext: string, testCallback: TestCa
             // TODO: Should this fail/warn if baselines have changed?
             _it(sourceBasename, function () {
                 const contents = fs.readFileSync(sourcePath, 'utf8');
-                const result = testCallback(contents);
+                const result = testCallback!(contents);
     
                 const json = JSON.stringify(result, null, 2);
                 fs.writeFileSync(outputPath, json);
             });
         }
     });
+}
+
+interface TestCaseOptions {
+    name: string;
+    casesPath: string;
+    outputBasePath: string;
+    ext: string;
+    testCallback: TestCallback;
 }
 
 type TestCallback = (contents: string) => any;
