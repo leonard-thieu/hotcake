dd636d496db0243f50e9628a10776aa5fdff55c7
Leonard Thieu <leonard.thieu@gmail.com>
2018-08-30 08:36:37 -0400

    `ExpressionStatement` should serialize its terminator.

--- src/Node/Statement/ExpressionStatement.ts
+++ src/Node/Statement/ExpressionStatement.ts
@@ -6,6 +6,7 @@ import { Statement } from './Statement';
 export class ExpressionStatement extends Statement {
     static CHILD_NAMES: (keyof ExpressionStatement)[] = [
         'expression',
+        'terminator',
     ];
 
     readonly kind = NodeKind.ExpressionStatement;
