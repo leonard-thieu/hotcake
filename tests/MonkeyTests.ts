ad93b7f18d7ef0292f1750983479fe0ce35bf04c
Leonard Thieu <leonard.thieu@gmail.com>
2019-08-17 07:17:01 -0400

    Ignore broken Monkey tests.

--- tests/MonkeyTests.ts
+++ tests/MonkeyTests.ts
@@ -1,9 +1,22 @@
 import fs = require('fs');
+import path = require('path');
 import { executeBinderTestCases, executeParserTestCases, executePreprocessorParserTestCases, executePreprocessorTokenizerTestCases, getCasePaths, getFrameworkDirectory } from './shared';
 
 const name = 'MonkeyX';
 const rootPath = getFrameworkDirectory();
 const casePaths = getCasePaths(rootPath).filter((casePath) => {
+    const relativePath = path.relative(rootPath, casePath);
+    switch (relativePath) {
+        case path.normalize('bananas/devolonter/matchup/fontmachine/bitmapfont.monkey'):    // False positive, has Function Main() in comment
+        case path.normalize('bananas/mak/gles20cube/gles20cube.monkey'):                    // Requires non-existent variable (jk)
+        case path.normalize('bananas/skn3/gamepad/gamepad.monkey'):                         // Requires non-existent module (mojo2)
+        case path.normalize('bananas/skn3/monkenstein/monkenstein.monkey'):                 // Requires non-existent variable (level)
+        case path.normalize('bananas/skn3/texteffect/texteffect.monkey'):                   // Requires non-existent function (print)
+        case path.normalize('modules/dom/mkwebgl.monkey'):                                  // Requires non-existent module (trans.system)
+        case path.normalize('src/rebuildall.monkey'):                                       // Cannot build on Windows
+            return false;
+    }
+
     const document = fs.readFileSync(casePath, 'utf8');
 
     return document.includes('Function Main()');
